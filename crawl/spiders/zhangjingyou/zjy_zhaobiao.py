783525b91d4f5a9e5cea22e7e08624b34043b24e
张京佑 <13603217363@163.com>
2019-10-22 14:02:55 +0800

    a

---
+++ crawl/spiders/zhangjingyou/zjy_zhaobiao.py
@@ -0,0 +1,18 @@
+import scrapy
+from scrapy import Selector,Request
+from urllib.parse import urljoin
+from crawl.items.lly_biao import ZhaobiaoItem
+
+
+class BiaoSpider(scrapy.Spider):
+    name = 'zjy_biao'
+    allowed_domains = []
+    start_urls = ['http://www.shenhuabidding.com.cn/bidweb/001/001002/1.html']
+
+    def parse(self, response):
+        sel=Selector(response)
+        urls=sel.css('li.right-item.clearfix div a::attr(href)')
+        urls=set(urls)
+        for i in list(urls):
+            print(i)
+
